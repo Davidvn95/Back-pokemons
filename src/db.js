0279e1f976ee0e9cde522a3a0922ac9250a0e5b3
David Vergara Navarro <davidvergaraok@gmail.com>
2023-08-31 17:04:06 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -6,15 +6,15 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
 } = process.env;
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
-
-const sequelize = new Sequelize(`postgres://pokedb:ernBEGmXoiDNMy51GxJrSq4148Uajmpn@dpg-cg8h6h64dad531s3ku40-a/pokemon_e54v`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+
+// const sequelize = new Sequelize(`postgres://pokedb:ernBEGmXoiDNMy51GxJrSq4148Uajmpn@dpg-cg8h6h64dad531s3ku40-a/pokemon_e54v`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
